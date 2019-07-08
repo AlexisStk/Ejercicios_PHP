478aaf70886e101c11f2355d5696e101123d425d
AlexisStk <alexis_r71@yahoo.com.ar>
2019-07-07 23:36:21 -0300

    mas erjercicios

--- php_practica/ejercitando-bucles.php
+++ php_practica/ejercitando-bucles.php
@@ -1,15 +1,33 @@
 <?php
+// Ejercicio 1 | imprime en pantalla numero del 1 al 100
 
+for ($i=1; $i <=100 ; $i++) { 
+    echo "$i <br>" ;
+}
 
-// $random = rand(0,100);
+//Ejercicio 2 | Imprime numero aleatorio entre el 0 al 100
 
-// for ($i=0; $i <= $random; $i++) { 
-//     echo $i . "<br>";
-// }
-// $a = 100;
-// while ($a >= 85) {
-//      echo $a-- . "<br>";
-// };
+echo "<hr>";
+
+ $random = rand(0,100);
+
+for ($i=0; $i <= $random; $i++) { 
+    echo $i . "<br>";
+}
+
+echo "<hr>";
+
+//Ejercicio 3 | Imprime numero en forma descendente
+
+$a = 100;
+while ($a >= 85) {
+     echo $a-- . "<br>";
+};
+
+echo "<hr>";
+ 
+//Ejercicio 4 tira un dado, el bucle se detiene cuando sale 5 veces cara
+
 $cara = 0;
 $tiros = 0;
 
@@ -23,8 +41,29 @@ while($cara<5){
 
 }
 
-echo "salio $cara veces cara y se tiro $tiros veces el dado";
+echo "Salio $cara veces cara y se tiro $tiros veces el dado";
 
+echo "<hr>";
+
+// Ejercicio 5 recorre el Array multidimencional y imprime en pantalla sus valores 
+
+$ceu = [
+    "Argentina" => ["Buenos Aires", "Córdoba", "Santa Fé"],
+    "Brasil" => ["Brasilia", "Rio de Janeiro", "Sao Pablo"],
+    "Colombia" => ["Cartagena", "Bogota", "Barranquilla"],
+    "Francia" => ["Paris", "Nantes", "Lyon"],
+    "Italia" => ["Roma", "Milan", "Venecia"],
+    "Alemania" => ["Munich", "Berlin", "Frankfurt"]
+    ];
+
+    foreach ($ceu as $pais => $provincias) {
+        echo "Las ciudades de $pais son:";
+        
+        foreach ($provincias as $valor) {
+            echo "<li>$valor</li>";
+        };
+    }
+ 
 
 
 
